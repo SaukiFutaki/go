faf924cbdbabcac83d2380bce5493e2776270e07
SaukiFutaki <utaki.sauki19@gmail.com>
2024-08-01 17:11:07 +0700

    const

---
+++ const.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name = "Sauki"
+	fmt.Println("My name is", name)
+
+	// ? Constant declaration boleh tidak digunakan
+	const (
+		age = 20
+		isMarried = false
+	)
+
+	var (
+		keren = true
+		anjay string
+	)
+
+	anjay = "anjay"
+	fmt.Println("Anjay", anjay)
+
+
+	fmt.Println("My age is", age)
+	fmt.Println("Is married?", isMarried)
+	fmt.Println("Keren?", keren)
+}
--- var.go
+++ var.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	var name = "Sauki"
+
 	
 	// ? Short declaration
 	hello := "Hello World"
 	fmt.Println(hello)
+	
+	fmt.Println("My name is", name)
+	name = "Budi"
 	fmt.Println("My name is", name)
 }
