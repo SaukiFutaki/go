429e6d2bb2dd54e139d5685fcd9296d0f99642d8
SaukiFutaki <utaki.sauki19@gmail.com>
2024-08-01 19:50:53 +0700

    switch case

---
+++ switch-case.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	month := 1
+	switch month {
+	case 1:
+		fmt.Println("January")
+	case 2:
+		fmt.Println("February")
+	case 3:
+		fmt.Println("March")
+	}
+}
